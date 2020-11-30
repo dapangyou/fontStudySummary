81562a7a0a03f375caa70bc2fbee2e50d2457357
xiaoxiaocute <1831731052@qq.com>
2020-12-01 00:22:46 +0800

    node

---
+++ 20201130/node/async/hello-generator.js
@@ -0,0 +1,13 @@
+function* func() { 
+    console.log('one')
+    yield '1'
+    console.log('two')
+    yield '2'
+    console.log('three')
+    yield '3'
+}
+
+const f = func();
+console.log('next:', f.next());
+console.log('next:', f.next());
+console.log('next:',f.next());
